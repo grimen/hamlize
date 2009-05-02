bf70092b58e454e30ac2f187570052a3f511d1e0
Jonas Grimfelt <grimen@gmail.com>
2009-05-02 12:30:01 +0200

    Fixed some bugs since refactoring.

--- lib/hamlize.rb
+++ lib/hamlize.rb
@@ -5,55 +5,51 @@ class Hamlize
   
   class HamlizeError < StandardError; end
   
-  def initialize(paths = '.', options = {})
-    options[:hamlize_extensions] ||= [:erb, :rhtml, :html]
-    options[:sassize_extensions] ||= [:css]
-    options[:recursive] = true if options[:recursive].nil?
-    options[:ignore_vendor] = true if options[:ignore_vendor].nil?
-    options[:with_output] = false if options[:with_output].nil?
-    options[:delete_after_conversion] = false if options[:delete_after_conversion].nil?
-    options.merge(:paths => paths.to_a)
+  def initialize(paths = '.', opt = {})
+    @options = opt
+    @options[:hamlize_extensions] ||= [:erb, :rhtml, :html]
+    @options[:sassize_extensions] ||= [:css]
+    @options[:recursive] = true if @options[:recursive].nil?
+    @options[:ignore_vendor] = true if @options[:ignore_vendor].nil?
+    @options[:with_output] = false if @options[:with_output].nil?
+    @options[:delete_after_conversion] = false if @options[:delete_after_conversion].nil?
+    @options.merge!(:paths => paths.to_a)
   end
   
   def convert!(to = [:haml, :sass])
-    hamlize! if to[:haml]
-    sassize! if to[:sass]
+    hamlize! if to.include?(:haml)
+    sassize! if to.include?(:sass)
   end
   
   def hamlize!(with_output = false)
+    puts "--------------------------------------"
+    puts " Convert to HAML"
+    puts "--------------------------------------"
     begin
-      query = build_file_match_expression(options[:paths], options[:hamlize_extensions], @options[:recursive])
-      Dir[file_match_string].each do |file|
-        html2haml!(file) unless file.match(/vendor\/[\w]+/)
+      query = build_file_match_expression(@options[:paths], @options[:hamlize_extensions], @options[:recursive])
+      Dir.glob(query).each do |file|
+        Hamlize.html2haml!(file) unless @options[:ignore_vendor] && file.match(/vendor\/[\w]+/)
       end
       @hamlize_result = true
-    rescue
+    rescue StandardError => e
+      puts "Error: #{e}"
       @hamlize_result = false
     end
   end
   
   def sassize!(with_output = false)
+    puts "--------------------------------------"
+    puts " Convert to SASS"
+    puts "--------------------------------------"
     begin
-      query = build_file_match_expression(options[:paths], options[:sassize_extensions], @options[:recursive])
-      Dir[query].each do |file|
-        html2haml!(file) unless file.match(/vendor\/[\w]+/)
+      query = build_file_match_expression(@options[:paths], @options[:sassize_extensions], @options[:recursive])
+      Dir.glob(query).each do |file|
+        Hamlize.css2sass!(file) unless @options[:ignore_vendor] && file.match(/vendor\/[\w]+/)
       end
       @sassize_result = true
     rescue
       @sassize_result = false
     end
-  end
-  
-  def self.html2haml!(source_file, with_output = false)
-    destination_file = "#{File.basename(file, '.*')}.haml"
-    `html2haml -rx #{source_file} #{destination_file}`
-    log_file_conversion(source_file, destination_file)
-  end
-  
-  def self.css2sass!(source_file, with_output = false)
-    destination_file = "#{File.basename(file, '.*')}.sass"
-    `css2sass -rx #{source_file} #{destination_file}`
-    log_file_conversion(source_file, destination_file)
   end
   
   def result(from = [:haml, :sass])
@@ -62,21 +58,24 @@ class Hamlize
     @result &= @sassize_result if from.include?(:sass)
   end
   
-  private
-  
-  def log_file_conversion(source_file, destination_file)
-    if File.exist?(destination_file)
-      "** Successfully generated: #{destination_file} (from #{source_file})"
-    else
-      "** Failed to generate: #{destination_file} (from #{source_file})"
-      raise HamlizeError.new("Failed to convert #{source_file}.")
-    end
+  def self.html2haml!(source_file, with_output = false)
+    destination_file = File.join(File.dirname(source_file), "#{File.basename(source_file, '.*')}.haml")
+    `html2haml -rx #{source_file} #{destination_file}`
+    Hamlize.log_file_conversion(source_file, destination_file)
   end
   
+  def self.css2sass!(source_file, with_output = false)
+    destination_file = File.join(File.dirname(source_file), "#{File.basename(source_file, '.*')}.sass")
+    `css2sass #{source_file} #{destination_file}`
+    Hamlize.log_file_conversion(source_file, destination_file)
+  end
+  
+  private
+  
   def build_file_match_expression(paths, extensions, recursive = true)
-    file_match_string = match_filter(options[:paths])
-    file_match_string = File.join(search_string, '**') if recursive == true
-    file_match_string = File.join(search_string, "*.#{match_filter(options[:hamlize_extensions])}")
+    file_match_string = match_filter(paths)
+    file_match_string = File.join(file_match_string, '**') if recursive
+    file_match_string = File.join(file_match_string, "*.#{match_filter(extensions)}")
   end
   
   def match_filter(string_literals, method = :pattern)
@@ -84,6 +83,15 @@ class Hamlize
       "(#{string_literals.to_a * '|'})"
     else
       "{#{string_literals.to_a * ','}}"
+    end
+  end
+  
+  def self.log_file_conversion(source_file, destination_file)
+    if File.exist?(destination_file)
+      puts "** Successfully generated: #{destination_file} (from #{source_file})"
+    else
+      puts "** Failed to generate: #{destination_file} (from #{source_file})"
+      raise HamlizeError.new("Failed to convert #{source_file}.")
     end
   end
   
